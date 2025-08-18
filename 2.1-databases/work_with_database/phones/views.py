1702b218a235efacf4a095c8d2d740d6ff95d1ed
Michael Ignatev <ignatevmc94@gmail.com>
2025-08-18 21:41:25 +0300

    Add files via upload

---
+++ 2.1-databases/work_with_database/phones/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+
+from phones.models import Phone
+
+
+def index(request):
+    return redirect('catalog')
+
+
+def show_catalog(request):
+    template = 'catalog.html'
+    sort = request.GET.get('sort', 'name')
+    if sort == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif sort == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:  # Сортировка по имени (по умолчанию)
+        phones = Phone.objects.all().order_by('name')
+    context = {'phones': phones}
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    template = 'product.html'
+    context = {'phone': Phone.objects.get(slug=slug)}
+    return render(request, template, context)
+
