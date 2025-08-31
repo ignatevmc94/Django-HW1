e5c2eb870e9ae323639ecb8f34abed9483176aff
Michael Ignatev <ignatevmc94@gmail.com>
2025-08-31 15:30:36 +0300

    Add files via upload

---
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+
+    object_list = Student.objects.order_by(ordering)
+    context = {'object_list': object_list,
+               }
+
+    return render(request, template, context)
